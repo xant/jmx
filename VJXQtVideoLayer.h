f7605f1e8826fe4545326317f2d05f20911ee7fa
xant <xant@dyne.org>
2010-09-05 21:34:30 +0200

    remaed VJXMovieLayer to VJXQtVideoLayer

---
+++ VJXQtVideoLayer.h
@@ -0,0 +1,28 @@
+//
+//  VJMovieLayer.h
+//  MoviePlayerC
+//
+//  Created by Igor Sutton on 8/5/10.
+//  Copyright (c) 2010 StrayDev.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <QTKit/QTKit.h>
+#import "VJXLayer.h"
+
+@interface VJXQtVideoLayer : VJXLayer {
+    QTMovie *movie;
+    NSString *moviePath;
+
+    BOOL paused;
+    BOOL repeat;
+}
+
+@property (retain) QTMovie *movie;
+@property (copy) NSString *moviePath;
+@property (assign) BOOL paused;
+@property (assign) BOOL repeat;
+
+- (void)loadMovie;
+
+@end

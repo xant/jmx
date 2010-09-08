2e478b4168a595710c01c39572b978b42dd2fc23
xant <xant@dyne.org>
2010-09-08 23:35:14 +0200

    include the license in all files

--- VJXBoardEntityPin.m
+++ VJXBoardEntityPin.m
@@ -5,6 +5,21 @@
 //  Created by Igor Sutton on 8/26/10.
 //  Copyright 2010 StrayDev.com. All rights reserved.
 //
+//  This file is part of VeeJay
+//
+//  VeeJay is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with VeeJay.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import "VJXBoardEntityPin.h"
 #import "VJXBoardDelegate.h"
@@ -31,6 +46,8 @@
         [connectors release];
     if (pin)
         [pin release];
+    if (tempConnector)
+        [tempConnector release];
     [super dealloc];
 }
 

e135c788a13b9e83cf2032e4b9c19e89e3d1cbc2
xant <xant@dyne.org>
2010-09-13 16:23:47 +0200

    new (still empty) files
    but already required by the project

---
+++ VJXQtExporter.h
@@ -0,0 +1,16 @@
+//
+//  VJXQtExporter.h
+//  VeeJay
+//
+//  Created by xant on 9/13/10.
+//  Copyright 2010 Dyne.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface VJXQtExporter : NSObject {
+
+}
+
+@end

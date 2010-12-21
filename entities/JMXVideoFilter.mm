a705d0f3272622516a1de3adf0f1de30e75ae782
xant <xant@xant.net>
2010-12-21 05:14:31 +0100

    license

--- entities/JMXVideoFilter.mm
+++ entities/JMXVideoFilter.mm
@@ -5,6 +5,21 @@
 //  Created by xant on 12/18/10.
 //  Copyright 2010 Dyne.org. All rights reserved.
 //
+//  This file is part of JMX
+//
+//  JMX is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with JMX.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #define __JMXV8__
 #import "JMXVideoFilter.h"

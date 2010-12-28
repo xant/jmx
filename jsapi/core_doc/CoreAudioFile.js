024f928fdfa72e0244235565735e1fb883cc3219
xant <xant@dyne.org>
2010-12-28 18:23:27 +0100

    leftover

---
+++ jsapi/core_doc/CoreAudioFile.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview
+ * Read samples from audio files
+ */
+
+/**
+ * CoreAudioFile
+ * @constructor
+ * @base AudioFile
+ * @class Read samples from audio files
+ * Supported file types can be obtained calling the {@link AudioFile#supportedFileTypes} method
+ * <h3>InputPins:</h3>
+ *  <ul>
+ *  <li>path {String}</li>
+ *  <li>repeat {Bool}</li>
+ *  <li>pause {Bool}</li>
+ *  </ul>
+ * <h3>OutputPins:</h3>
+ *  <ul>
+ *  <li>audio {Audio}</li>
+ *  </ul>
+ *
+ */
+function CoreAudioFile()
+{
+    this.path = "";
+    this.repeat = false;
+    this.paused = false;
+
+    /**
+     * Open audio file at specified path
+     */
+    this.open = function(path) { }
+
+    /**
+     * Close the current audio file (if any)
+     */
+    this.close = function() { }
+
+    /**
+     * The frequency of the entity.
+     * @type float
+     */
+    this.frequency = 0;
+    
+    /**
+     * Start the entity
+     * @addon
+     */
+    this.start = function() { }
+    
+    /**
+     * Stop the entity
+     */
+    this.stop = function() { }
+    
+    // ... 
+}
+
+/**
+ * Returns a list of supported audio file types
+ */
+CoreAudioFile.supportedFileTypes = function() {
+    // ...
+}
+

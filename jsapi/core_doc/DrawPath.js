2300834ac72bab85ce99994bc3f0cf1aad60c42d
xant <xant@dyne.org>
2012-02-20 16:43:06 +0100

    documentation

--- jsapi/core_doc/DrawPath.js
+++ jsapi/core_doc/DrawPath.js
@@ -58,6 +58,271 @@ function DrawPath(width, height)
      * Stop the entity
      */
     this.stop = function() { }
+    /***
+     * save
+     * TODO: Document
+     */
+    this.save = function() { }
+
+    /***
+     * restore
+     * TODO: Document
+     */
+    this.restore = function() { }
+
+    /***
+     * scale
+     * TODO: Document
+     */
+    this.scale = function() { }
+
+    /***
+     * rotate
+     * TODO: Document
+     */
+    this.rotate = function() { }
+
+    /***
+     * translate
+     * TODO: Document
+     */
+    this.translate = function() { }
+
+    /***
+     * transform
+     * TODO: Document
+     */
+    this.transform = function() { }
+
+    /***
+     * setTransform
+     * TODO: Document
+     */
+    this.setTransform = function() { }
+
+    /***
+     * createLinearGradient
+     * TODO: Document
+     */
+    this.createLinearGradient = function() { }
+
+    /***
+     * createRadialGradient
+     * TODO: Document
+     */
+    this.createRadialGradient = function() { }
+
+    /***
+     * createPattern
+     * TODO: Document
+     */
+    this.createPattern = function() { }
+
+    /***
+     * clearRect
+     * TODO: Document
+     */
+    this.clearRect = function() { }
+
+    /***
+     * fillRect
+     * TODO: Document
+     */
+    this.fillRect = function() { }
+
+    /***
+     * strokeRect
+     * TODO: Document
+     */
+    this.strokeRect = function() { }
+
+    /***
+     * beginPath
+     * TODO: Document
+     */
+    this.beginPath = function() { }
+
+    /***
+     * closePath
+     * TODO: Document
+     */
+    this.closePath = function() { }
+
+    /***
+     * moveTo
+     * TODO: Document
+     */
+    this.moveTo = function() { }
+
+    /***
+     * lineTo
+     * TODO: Document
+     */
+    this.lineTo = function() { }
+
+    /***
+     * quadraticCurveTo
+     * TODO: Document
+     */
+    this.quadraticCurveTo = function() { }
+
+    /***
+     * bezierCurveTo
+     * TODO: Document
+     */
+    this.bezierCurveTo = function() { }
+
+    /***
+     * arcTo
+     * TODO: Document
+     */
+    this.arcTo = function() { }
+
+    /***
+     * rect
+     * TODO: Document
+     */
+    this.rect = function() { }
+
+    /***
+     * arc
+     * TODO: Document
+     */
+    this.arc = function() { }
+
+    /***
+     * fill
+     * TODO: Document
+     */
+    this.fill = function() { }
+
+    /***
+     * stroke
+     * TODO: Document
+     */
+    this.stroke = function() { }
+
+    /***
+     * clip
+     * TODO: Document
+     */
+    this.clip = function() { }
+
+    /***
+     * isPointInPath
+     * TODO: Document
+     */
+    this.isPointInPath = function() { }
+
+    /***
+     * drawFocusRing
+     * TODO: Document
+     */
+    this.drawFocusRing = function() { }
+
+    /***
+     * drawImage
+     * TODO: Document
+     */
+    this.drawImage = function() { }
+
+    /***
+     * strokeText
+     * TODO: Document
+     */
+    this.strokeText = function() { }
+
+    /***
+     * measureText
+     * TODO: Document
+     */
+    this.measureText = function() { }
+
+    /***
+     * fillText
+     * TODO: Document
+     */
+    this.fillText = function() { }
+
+    /***
+     * getImageData
+     * TODO: Document
+     */
+    this.getImageData = function() { }
+
+    /***
+     * createImageData
+     * TODO: Document
+     */
+    this.createImageData = function() { }
+
+    /***
+     * putImageData
+     * TODO: Document
+     */
+    this.putImageData = function() { }
+
+
+    /***
+     * globalAlpha
+     * TODO: Document
+     */
+    this.globalAlpha = null;
+
+    /***
+     * fillStyle
+     * TODO: Document
+     */
+    this.fillStyle = null;
+
+    /***
+     * strokeStyle
+     * TODO: Document
+     */
+    this.strokeStyle = null;
+
+    /***
+     * globalCompositeOperation
+     * TODO: Document
+     */
+    this.globalCompositeOperation = null;
+
+    /***
+     * font
+     * TODO: Document
+     */
+    this.font = null;
+
+    /***
+     * lineWidth
+     * TODO: Document
+     */
+    this.lineWidth = null;
+
+    /***
+     * shadowColor
+     * TODO: Document
+     */
+    this.shadowColor = null;
+
+    /***
+     * shadowOffsetX
+     * TODO: Document
+     */
+    this.shadowOffsetX = null;
+
+    /***
+     * shadowOffsetY
+     * TODO: Document
+     */
+    this.shadowOffsetY = null;
+
+    /***
+     * shadowBlur
+     * TODO: Document
+     */
+    this.shadowBlur = null;
+
 }
 
 

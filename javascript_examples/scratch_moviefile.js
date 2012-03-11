828796c06385cd82390b9a6f22d3086c99c3f134
xant <xant@dyne.org>
2012-03-11 22:42:36 +0100

    control saturation with the vertical mouse coordinate

--- javascript_examples/scratch_moviefile.js
+++ javascript_examples/scratch_moviefile.js
@@ -1,11 +1,21 @@
-m = new MovieFile('/Users/xant/sample_iTunes.mov');
-v = new VideoOutput(m.size.width, m.size.height);
+width = 640;
+height = 480;
+
+m = new MovieFile('/Applications/Adobe Photoshop CS5.1/Samples/CheeziPuffs.mov');
+m.scaleRatio = (m.size.width > m.size.height)
+               ? width / m.size.width
+               : height / m.size.height;
+
+v = new VideoOutput(width, height);
+a = new AudioOutput();
 
 m.output.frame.connect(v.input.frame);
+m.output.audio.connect(a.input.audio);
+
 step = v.size.width / m.duration;
 
 document.addEventListener("mousemove", function(e) {
     time = e.screenX/step;
     m.seekAbsoluteTime(time);
+    m.saturation = v.size.height / e.screenY;
 }, false);
-

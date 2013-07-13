0da9b1e9a5f876b30b8d0eb2e48e3d58033bcef9
xant <xant@xant.net>
2013-07-13 02:19:59 +0200

    leftover while debugging

--- entities/JMXAudioSpectrumAnalyzer.h
+++ entities/JMXAudioSpectrumAnalyzer.h
@@ -10,7 +10,7 @@
 #import <AudioToolbox/AudioConverter.h>
 #import "JMXEntity.h"
 
-#define kJMXAudioSpectrumNumFrequencies 512
+#define kJMXAudioSpectrumNumFrequencies 14
 #define kJMXAudioSpectrumImageBufferCount 32
 
 @class JMXSpectrumAnalyzer;
